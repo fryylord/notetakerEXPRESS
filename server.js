132d5c65fc5068fe089588fc68e97a7292495d31
fryylord <rob@mchays.com>
2022-11-29 14:09:29 -0600

    added use functions for resource data

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 // Sets port for listening
 const PORT = process.env.PORT || 8001;
 
+
+//use functions for resource data
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Listening function for port
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
