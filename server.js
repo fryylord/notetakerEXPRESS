885d05989123851b8b403935eda815a1d0121fb9
fryylord <rob@mchays.com>
2022-11-29 14:01:54 -0600

    ceated server.js with dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Sets port for listening
+const PORT = process.env.PORT || 8001;
